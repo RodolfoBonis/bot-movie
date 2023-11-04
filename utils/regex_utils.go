3c2c2073e651194471cf9a8ed58b241b0295ff98
D'Bonis <dev@rodolfodebonis.com.br>
2023-11-04 03:39:17 -0300

    fix: fixed compare strings with regex and lower case

--- utils/regex_utils.go
+++ utils/regex_utils.go
@@ -1,11 +1,55 @@
 package utils
 
 import (
-	"golang.org/x/text/unicode/norm"
+	"regexp"
 	"strings"
 )
 
 func NormalizeAndLowercase(s string) string {
-	normalized := norm.NFD.String(s)
+	normalized := removeAccents(s)
+
 	return strings.ToLower(normalized)
+}
+
+func removeAccents(text string) string {
+	accents := map[string]string{
+		"á": "a",
+		"à": "a",
+		"â": "a",
+		"ã": "a",
+		"é": "e",
+		"è": "e",
+		"ê": "e",
+		"í": "i",
+		"ì": "i",
+		"î": "i",
+		"ó": "o",
+		"ò": "o",
+		"ô": "o",
+		"õ": "o",
+		"ú": "u",
+		"ù": "u",
+		"û": "u",
+		"ç": "c",
+	}
+
+	pattern := "[" + strings.Join(getKeys(accents), "") + "]"
+
+	regex := regexp.MustCompile(pattern)
+
+	textWithoutAccents := regex.ReplaceAllStringFunc(text, func(match string) string {
+		return accents[match]
+	})
+
+	return textWithoutAccents
+}
+
+func getKeys(m map[string]string) []string {
+	keys := make([]string, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
 }
